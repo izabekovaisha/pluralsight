059e947093815a869227d3b3cde985b41bcf8c7e
izabekovaisha <izabekovaisha@gmail.com>
2024-05-13 09:14:58 -0400

    Update HotelOperationsModified2

--- java-development/workbook-4/HotelOperationsModified2/src/main/java/com/pluralsight/Hotel.java
+++ java-development/workbook-4/HotelOperationsModified2/src/main/java/com/pluralsight/Hotel.java
@@ -1,6 +1,7 @@
 package com.pluralsight;
 
-public class Hotel {private String name;
+public class Hotel {
+    private String name;
     private int numberOfSuites;
     private int numberOfRooms;
     private int bookedSuites;
