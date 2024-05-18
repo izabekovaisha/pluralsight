383a21df1503c1d8ab34f7bcc9102f648c9ca480
izabekovaisha <izabekovaisha@gmail.com>
2024-05-18 19:46:30 -0400

    Add Vehicles

--- java-development/workbook-5/Vehicles/src/main/java/com/pluralsight/Hovercraft.java
+++ java-development/workbook-5/Vehicles/src/main/java/com/pluralsight/Hovercraft.java
@@ -1,26 +1,17 @@
 package com.pluralsight;
 
-public class Hovercraft extends Vehicle {
+public class HoverCraft extends Vehicle {
+    private int airCushionPressure;
 
-        private int airCushionPressure;
-
-        public Hovercraft(String model, String color, int topSpeed, int numberOfPassengers, int fuelCapacity, int cargoCapacity, int airCushionPressure) {
-            super(model, color, topSpeed, numberOfPassengers, fuelCapacity, cargoCapacity);
-            this.airCushionPressure = airCushionPressure;
-        }
-
-        public int getAirCushionPressure() {
-            return airCushionPressure;
-        }
-
-        public void setAirCushionPressure(int airCushionPressure) {
-            this.airCushionPressure = airCushionPressure;
-        }
-
-        public void hover() {
-            System.out.println("Hovercraft is hovering.");
-        }
+    public int getAirCushionPressure() {
+        return airCushionPressure;
     }
 
+    public void setAirCushionPressure(int airCushionPressure) {
+        this.airCushionPressure = airCushionPressure;
+    }
 
+    public void hover() {
+        System.out.println("Hovercraft is hovering.");
+    }
 }
--- java-development/workbook-5/Vehicles/src/main/java/com/pluralsight/Main.java
+++ java-development/workbook-5/Vehicles/src/main/java/com/pluralsight/Main.java
@@ -1,27 +1,33 @@
 package com.pluralsight;
 
 public class Main {
-     public static void main(String[] args) {
-          Moped slowRide = new Moped("Moped Model", "Red", 50, 5, 1, 10, 20);
-          SemiTruck semiTruck = new SemiTruck("Semitruck Model", );
+    public static void main(String[] args) {
+        Moped slowRide = new Moped();
+        slowRide.setColor("Red");
+        slowRide.setFuelCapacity(5);
 
+        SemiTruck semiTruck = new SemiTruck();
+        semiTruck.setNumberOfTrailers(2);
+        semiTruck.setFuelCapacity(100);
 
-          Hovercraft hoverCraft = new Hovercraft();
+        HoverCraft hoverCraft = new HoverCraft();
+        hoverCraft.setAirCushionPressure(50);
+        hoverCraft.setColor("Blue");
 
+        Car car = new Car();
+        car.setNumberOfDoors(4);
+        car.setTopSpeed(200);
 
-          Car car = new Car();
+        // Test the functionality of the classes and methods
+        slowRide.ride();
+        semiTruck.loadCargo();
+        hoverCraft.hover();
+        car.openTrunk();
 
-
-          // Test the functionality of the classes and methods
-          slowRide.ride();
-          semiTruck.loadCargo();
-          hoverCraft.hover();
-          car.openTrunk();
-
-          // Accessing Vehicle getters
-          System.out.println("Color of the Moped: " + slowRide.getColor());
-          System.out.println("Fuel capacity of the SemiTruck: " + semiTruck.getFuelCapacity());
-          System.out.println("Air cushion pressure of the HoverCraft: " + hoverCraft.getAirCushionPressure());
-          System.out.println("Top speed of the Car: " + car.getTopSpeed());
-     }
+        // Accessing Vehicle getters
+        System.out.println("Color of the Moped: " + slowRide.getColor());
+        System.out.println("Fuel capacity of the SemiTruck: " + semiTruck.getFuelCapacity());
+        System.out.println("Air cushion pressure of the HoverCraft: " + hoverCraft.getAirCushionPressure());
+        System.out.println("Top speed of the Car: " + car.getTopSpeed());
+    }
 }
