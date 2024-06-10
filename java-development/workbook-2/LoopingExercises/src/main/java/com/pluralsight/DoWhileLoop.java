4dc2e4d24fea8e7e5e4f79111f1d904c61b16207
izabekovaisha <izabekovaisha@gmail.com>
2024-06-09 21:16:26 -0400

    Updated LoopingExercises

--- java-development/workbook-2/LoopingExercises/src/main/java/com/pluralsight/DoWhileLoop.java
+++ java-development/workbook-2/LoopingExercises/src/main/java/com/pluralsight/DoWhileLoop.java
@@ -2,16 +2,10 @@ package com.pluralsight;
 
 public class DoWhileLoop {
     public static void main(String[] args) {
-            int i = 1;
-            int sum = 0;
-
-            do {
-                sum += 1;
-                i ++;
-
-                System.out.println("I love Java");
-
-            } while (i <= 5);
-
+        int count = 0;
+        do {
+            System.out.println("I love Java");
+            count++;
+        } while (count < 5);
     }
 }
--- java-development/workbook-2/LoopingExercises/src/main/java/com/pluralsight/WhileLoop.java
+++ java-development/workbook-2/LoopingExercises/src/main/java/com/pluralsight/WhileLoop.java
@@ -2,13 +2,10 @@ package com.pluralsight;
 
 public class WhileLoop {
     public static void main(String[] args) {
-        int i = 0;
-        while (i <5) {
+        int count = 0;
+        while (count < 5) {
             System.out.println("I love Java");
-            i++;
-
+            count++;
         }
-
-
-        }
+    }
 }
