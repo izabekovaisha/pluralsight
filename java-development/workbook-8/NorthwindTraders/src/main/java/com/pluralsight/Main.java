ae1d865cb86434d3a777167b1041968963b6f1bb
izabekovaisha <izabekovaisha@gmail.com>
2024-06-11 11:44:31 -0400

    Modified

--- java-development/workbook-8/NorthwindTraders/src/main/java/com/pluralsight/Main.java
+++ java-development/workbook-8/NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -1,28 +1,51 @@
 package com.pluralsight;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
+        try {
+            if (args.length != 2) {
+                System.out.println("This application needs a Username and a Password to run");
+                System.exit(1);
+            }
 
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind",
-                "root",
-                "KakoitoParol123!");
+            String username = args[0];
+            String password = args[1];
 
-        Statement statement = connection.createStatement();
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
-        String query = "SELECT ProductName FROM products";
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind",
+                    username,
+                    password
+            );
 
-        ResultSet results = statement.executeQuery(query);
+            String query = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products";
 
-        while (results.next()) {
-            String productName = results.getString("ProductName");
-            System.out.println(productName);
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            ResultSet results = preparedStatement.executeQuery(query);
+
+            while (results.next()) {
+                String productID = results.getString("ProductID");
+                String productName = results.getString("ProductName");
+                String unitPrice = results.getString("UnitPrice");
+                String unitsInStock = results.getString("UnitsInStock");
+
+                System.out.println("ProductID: " + productID);
+                System.out.println("ProductName: " + productName);
+                System.out.println("UnitPrice: " + unitPrice);
+                System.out.println("UnitsInStock: " + unitsInStock);
+                System.out.println("=========================");
+            }
+            results.close();
+            preparedStatement.close();
+            connection.close();
+        } catch (Exception e) {
+            System.out.println("Error has occurred");
         }
-
-        connection.close();
-
     }
 }
+
 
