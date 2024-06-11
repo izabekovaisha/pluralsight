4539c0cd93f96bf5ba4f7bfc8806ab7e1088c5b7
izabekovaisha <izabekovaisha@gmail.com>
2024-06-11 11:47:50 -0400

    Delete scanner

--- java-development/workbook-8/NorthwindTraders/src/main/java/com/pluralsight/Main.java
+++ java-development/workbook-8/NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,6 @@
 package com.pluralsight;
 
 import java.sql.*;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
