847dd0528de4a55e4e404048593e7653b06f1232
izabekovaisha <izabekovaisha@gmail.com>
2024-06-13 22:53:20 -0400

    Updated NorthwindTraders

--- java-development/workbook-8/NorthwindTraders/src/main/java/com/pluralsight/Main.java
+++ java-development/workbook-8/NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -4,47 +4,46 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        try {
-            if (args.length != 2) {
-                System.out.println("This application needs a Username and a Password to run");
-                System.exit(1);
-            }
-
-            String username = args[0];
-            String password = args[1];
-
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind",
-                    username,
-                    password
-            );
-
-            String query = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products";
-
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-
-            ResultSet results = preparedStatement.executeQuery(query);
-
-            while (results.next()) {
-                String productID = results.getString("ProductID");
-                String productName = results.getString("ProductName");
-                String unitPrice = results.getString("UnitPrice");
-                String unitsInStock = results.getString("UnitsInStock");
-
-                System.out.println("ProductID: " + productID);
-                System.out.println("ProductName: " + productName);
-                System.out.println("UnitPrice: " + unitPrice);
-                System.out.println("UnitsInStock: " + unitsInStock);
-                System.out.println("=========================");
-            }
-            results.close();
-            preparedStatement.close();
-            connection.close();
-        } catch (Exception e) {
-            System.out.println("Error has occurred");
+        // Check for command line arguments for username and password
+        if (args.length != 2) {
+            System.out.println("Application needs two arguments to run: " +
+                    "java com.pluralsight.Main <username> <password>");
+            System.exit(1);
         }
+
+        String username = args[0];
+        String password = args[1];
+
+        // Load the MySQL Driver
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        // 1. Open a connection to the database
+        // Use the database URL to point to the correct database
+        Connection connection;
+        connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/northwind",
+                username,
+                password);
+
+        // Create statement
+        // The statement is tied to the open connection
+        Statement statement = connection.createStatement();
+
+        // Define your query
+        String query = "SELECT ProductName FROM products";
+
+        // 2. Execute your query
+        ResultSet results = statement.executeQuery(query);
+
+        // Process the results
+        while (results.next()) {
+            String productName = results.getString("ProductName");
+            System.out.println(productName);
+        }
+
+        // 3. Close the connection
+        connection.close();
+
+
     }
 }
-
-
